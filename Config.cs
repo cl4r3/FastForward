6c14f0d2a16f3eb8b144cece992d4687ecf52b61
Clare <ccathanlon@gmail.com>
2024-09-28 14:53:28 -0400

    initial upload

---
+++ Config.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Locations;
+
+namespace FastForward
+{
+    public class ModConfig
+    {
+        public SButton button1 = (SButton)192;
+        public SButton button2 = (SButton)186;
+        public int speed1 { get; set; } = 7;
+        public int speed2 { get; set; } = 2;
+    }
+
+    class ConfigMenu
+    {
+        private readonly IModHelper Helper;
+        private readonly IManifest ModManifest;
+
+        private ModConfig Config
+        {
+            get => ModEntry.Config;
+            set => ModEntry.Config = value;
+        }
+
+        public ConfigMenu(IMod mod)
+        {
+            Helper = mod.Helper;
+            ModManifest = mod.ModManifest;
+        }
+
+        public void RegisterMenu()
+        {
+            var GMCM = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (GMCM is not null)
+            {
+                GMCM.Register(ModManifest, () => Config = new ModConfig(), () => Helper.WriteConfig(Config));
+
+                GMCM.AddKeybind(ModManifest, () => Config.button1, (SButton val) => Config.button1 = val, () => "Button 1", () => "Button that toggles Speed 1");
+                GMCM.AddNumberOption(ModManifest, () => Config.speed1, (int val) => Config.speed1 = val, () => "Speed 1", () => "Speed multiplier toggled by Button 1", min: 2);
+                GMCM.AddKeybind(ModManifest, () => Config.button2, (SButton val) => Config.button2 = val, () => "Button 2", () => "Button that toggles Speed 2");
+                GMCM.AddNumberOption(ModManifest, () => Config.speed2, (int val) => Config.speed2 = val, () => "Speed 2", () => "Speed multiplier toggled by Button 2", min: 2);
+            }
+        }
+    }
+}
---
+++ ModEntry.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Characters;
+
+namespace FastForward
+{
+    internal sealed class ModEntry : Mod
+    {
+        internal static IMonitor ModMonitor { get; set; }
+        internal new static IModHelper Helper { get; set; }
+        internal static ModConfig Config;
+        private ConfigMenu ConfigMenu;
+
+        public static int curr_speed = 0;
+        public static bool locked;
+
+        public override void Entry(IModHelper helper)
+        {
+            ModMonitor = Monitor;
+            Helper = helper;
+            ConfigMenu = new ConfigMenu(this);
+
+            Helper.Events.GameLoop.GameLaunched += OnGameLaunched;
+            Helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
+            Helper.Events.Input.ButtonPressed += OnButtonPressed;
+            //OnUpdateTicking added/removed via button press
+        }
+
+        private void OnGameLaunched(object sender, GameLaunchedEventArgs e)
+        {
+            Config = Helper.ReadConfig<ModConfig>();
+            ConfigMenu.RegisterMenu();
+        }
+
+        private void OnSaveLoaded(object sender, EventArgs ex)
+        {
+            try
+            {
+                Config = Helper.ReadConfig<ModConfig>();
+            }
+            catch (Exception e)
+            {
+                ModMonitor.Log($"Fast Forward: Failed to load config settings. Will use default settings instead. Error details:\n{e}", LogLevel.Debug);
+                Config = new ModConfig();
+            }
+        }
+
+            private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            if (e.Button == Config.button1)
+            {
+                if (curr_speed == 0)
+                {
+                    curr_speed = 1;
+                    Helper.Events.GameLoop.UpdateTicking += OnUpdateTicking;
+                    Game1.addHUDMessage(new HUDMessage($"Fast Forward X{Config.speed1} enabled", 2));
+                }
+                else
+                {
+                    curr_speed = 0;
+                    Helper.Events.GameLoop.UpdateTicking -= OnUpdateTicking;
+                    Game1.addHUDMessage(new HUDMessage("Fast Forward Disabled", 3));
+                }
+            }
+            else if (e.Button == Config.button2)
+            {
+                if (curr_speed == 0)
+                {
+                    curr_speed = 2;
+                    Helper.Events.GameLoop.UpdateTicking += OnUpdateTicking;
+                    Game1.addHUDMessage(new HUDMessage($"Fast Forward X{Config.speed2} enabled", 2));
+                }
+                else
+                {
+                    curr_speed = 0;
+                    Helper.Events.GameLoop.UpdateTicking -= OnUpdateTicking;
+                    Game1.addHUDMessage(new HUDMessage("Fast Forward Disabled", 3));
+                }
+            }
+        }
+
+        private void OnUpdateTicking(object sender, UpdateTickingEventArgs e)
+        {
+            if (locked)
+                return;
+            locked = true;
+            for (int i = 0; i < (curr_speed == 1 ? Config.speed1 : Config.speed2); i++)
+            {
+                Helper.Reflection.GetMethod(Game1.game1, "Update").Invoke([Game1.currentGameTime]);
+            }
+            locked = false;
+        }
+    }
+}
